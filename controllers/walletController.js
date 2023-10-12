402158be00901e9b8fd4fa18d56839e211347f48
Mohit Saud <mohit@prabidhilabs.com>
2023-10-12 07:25:59 +0545

    Initialize :: Wallet Controller

---
+++ controllers/walletController.js
@@ -0,0 +1,13 @@
+const Wallet = require('../models/Wallet');
+
+exports.addWalletAddress = async (req, res) => {};
+
+exports.getLoggedInUserWalletAddresses = async (req, res) => {};
+
+exports.getAllWalletAddresses = async (req, res) => {};
+
+exports.getWalletAddress = async (req, res) => {};
+
+exports.updateWalletAddress = async (req, res) => {};
+
+exports.deleteWalletAddresses = async (req, res) => {};
