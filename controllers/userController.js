c1658fc0a44e6977806e1c118046ab10c1e025f7
Mohit Saud <mohit@prabidhilabs.com>
2023-10-13 16:17:39 +0545

    User :: Get User, Get Wallet Balance Update User

---
+++ controllers/userController.js
@@ -0,0 +1,71 @@
+const User = require('../models/User');
+const WalletAddress = require('../models/WalletAddress');
+
+const CustomError = require('../errors');
+const { StatusCodes } = require('http-status-codes');
+
+const { createTokenUser } = require('../utils');
+const {
+  getwalletBalanceForLoggedinUser,
+} = require('../utility-scripts/changes');
+
+exports.showCurrentLoggedInUser = async (req, res) => {
+  res.status(StatusCodes.OK).json({ user: req.user });
+};
+
+// @desc Get Single User
+// @route /api/v1/users/:id
+// @access Private
+exports.getSingleUser = async (req, res) => {
+  const { id: userId } = req.params;
+
+  const user = await User.findOne({ _id: userId }).select(
+    '-password -createdAt -updatedAt -__v'
+  );
+
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user found with id : ${userId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ user });
+};
+
+exports.updateUser = async (req, res) => {
+  const { userId } = req.user;
+
+  const user = await User.findOneAndUpdate(
+    {
+      _id: userId,
+    },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user found with id : ${userId}`);
+  }
+
+  const tokenUser = createTokenUser(user);
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+
+exports.getWalletBalanceForLoggedinUser = async (req, res) => {
+  const userId = req.user.userId;
+
+  const { dateRange } = req.query;
+
+  const walletAddresses = await WalletAddress.find({ user: userId }).select(
+    'address'
+  );
+
+  const addressessArray = walletAddresses.map((wallet) => wallet.address);
+
+  const results = await getwalletBalanceForLoggedinUser(
+    userId,
+    addressessArray,
+    dateRange
+  );
+
+  res.status(StatusCodes.OK).json(results);
+};
