d936a414040b83800879d3a7ab4cdae3eb2ae893
Mohit Saud <mohit@prabidhilabs.com>
2023-10-13 16:28:16 +0545

    Import :: roouters, setupcronjob using serInterval

--- app.js
+++ app.js
@@ -6,18 +6,23 @@ const notFoundMiddleware = require('./middleware/not-found');
 
 const authRouter = require('./routes/authRoutes');
 const walletRouter = require('./routes/walletRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // express
 const express = require('express');
 const app = express();
 
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 // database
 const connectDB = require('./db/connect');
+const setupCronJob = require('./utility-scripts/balance-fetch');
 
 app.use(morgan('tiny'));
+
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 // routes
 app.get('/', (req, res) => {
@@ -27,6 +32,7 @@ app.get('/', (req, res) => {
 // mount the routers
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/wallets', walletRouter);
+app.use('/api/v1/users', userRouter);
 
 // error handling
 app.use(notFoundMiddleware);
@@ -37,6 +43,10 @@ const PORT = process.env.PORT || 8000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+
+    // Set up the cron job after the database connection is established
+    setupCronJob();
+
     app.listen(
       PORT,
       console.log(`The server is listening on the port ${PORT}`)
